45477bb28ee7f766470d77f6348f6a751251b653
sudj <sudj@rose-hulman.edu>
2019-01-20 16:04:18 -0500

    DONE

--- src/m6_mutation.py
+++ src/m6_mutation.py
@@ -129,10 +129,18 @@ def RETURN_replace_negatives_by_zeros(numbers):
 
     Precondition: The argument is a list of numbers.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+    # DONE: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
+    sequence = []
+    for k in range(len(numbers)):
+        if numbers[k] < 0:
+            sequence = sequence + [0]
+        else:
+            sequence = sequence + [numbers[k]]
+    return sequence
+
 
 
 
@@ -186,7 +194,7 @@ def MUTATE_replace_negatives_by_zeros(numbers):
 
     Precondition: The argument is a list of numbers.
     """
-    # TODO: 3. First, READ THE ABOVE TEST CODE.
+    # DONE: 3. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
@@ -195,5 +203,9 @@ def MUTATE_replace_negatives_by_zeros(numbers):
 # If this module is running at the top level (as opposed to being
 # imported by another module), then call the 'main' function.
 # ----------------------------------------------------------------------
+    for k in range(len(numbers)):
+        if numbers[k] < 0:
+            numbers[k] = 0
+
 if __name__ == '__main__':
     main()
